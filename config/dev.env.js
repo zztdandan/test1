d36b17d69396218f5fe0799fead16fbc3492c7c8
80800275 <pw33@sina.com>
2019-03-04 09:21:06 +0800

    自定义mock

---
+++ config/dev.env.js
@@ -0,0 +1,172 @@
+"use strict";
+const Mock = require("mockjs");
+const path = require("path");
+var fs = require("fs");
+
+const loginMock = function(req, res) {
+  if (process.env.mockLogin && req.url.indexOf("/api/sys/login") === 0) {
+    res.json({
+      code: 0,
+      msg: "成功",
+      reqId: "dd" + new Date().getTime(),
+      data: {
+        token: "pw33test-@id()"
+      }
+    });
+    return true;
+  }
+};
+module.exports = {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://vue-admin"',
+  proxyTable: {
+    "/api/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      target: "http://172.16.4.146", // 真正测试服务器路径
+      // target: "http://127.0.0.1:8888", // 本地服务器路径，已取消，使用mock代替
+      // target: 'http://172.16.4.194:8080/app/mock/21', //远端rap2模拟服务器路径
+      pathRewrite: {
+        // "^/api/": "/",
+      }, // 重写路径
+
+      // changeOrigin: true,
+      bypass(req, res, proxyOptions) {
+        // 该函数必须返回false或返回被部署的文件路径，返回false继续。
+        const key = req.method.toUpperCase() + " " + req.url;
+        const urlPath = req.url.split("?")[0];
+        const mockPath = path.resolve(__dirname, "../mock/");
+        const fileName = path.resolve(mockPath, "./" + urlPath + ".json");
+        console.log("----------------request----------------", key);
+        // 是否需要本地模拟登陆
+        if (loginMock(req, res)) {
+          return true;
+        }
+        if (fs.existsSync(fileName)) {
+          var data = fs.readFileSync(fileName, "utf8"),
+            json = JSON.parse(data),
+            mockData = Mock.mock(json);
+          mockData.reqId = "mock-" + new Date().getTime();
+          console.log(
+            "--------------------mock----------------------\n",
+            fileName,
+            "mockData",
+            mockData
+          );
+          res.json(mockData);
+          return true;
+        }
+        return false;
+      }
+    },
+    "/mockxx/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      // target: "http://172.16.4.194:8080/app/mock/21", // 本地理服务器路径
+      target: "http://localhost:8080", // 本地理服务器路径
+      // target: 'http://140.143.26.135:3000', //远端 代理服务器路径
+      pathRewrite: {
+        "^/mockxx/": "/"
+      } // 重写路径
+      //   changeOrigin: true
+    }
+  },
+  proxyTableLocal: {
+    "/api/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      // target: "http://172.16.4.146", // 真正测试服务器路径
+      target: "http://127.0.0.1:8080", // 本地服务器路径，已取消，使用mock代替
+      // target: 'http://172.16.4.194:8080/app/mock/21', //远端rap2模拟服务器路径
+      pathRewrite: {
+        "^/api/": "/"
+      }, // 重写路径
+
+      // changeOrigin: true,
+      bypass(req, res, proxyOptions) {
+        // 该函数必须返回false或返回被部署的文件路径，返回false继续。
+        const key = req.method.toUpperCase() + " " + req.url;
+        const urlPath = req.url.split("?")[0];
+        const mockPath = path.resolve(__dirname, "../mock/");
+        const fileName = path.resolve(mockPath, "./" + urlPath + ".json");
+        console.log("----------------request----------------", key);
+        // 是否需要本地模拟登陆
+        if (loginMock(req, res)) {
+          return true;
+        }
+        if (fs.existsSync(fileName)) {
+          var data = fs.readFileSync(fileName, "utf8"),
+            json = JSON.parse(data),
+            mockData = Mock.mock(json);
+          mockData.reqId = "mock-" + new Date().getTime();
+          console.log(
+            "--------------------mock----------------------\n",
+            fileName,
+            "mockData",
+            mockData
+          );
+          res.json(mockData);
+          return true;
+        }
+        return false;
+      }
+    },
+    "/mockxx/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      // target: "http://172.16.4.194:8080/app/mock/21", // 本地理服务器路径
+      target: "http://localhost:8080", // 本地理服务器路径
+      // target: 'http://140.143.26.135:3000', //远端 代理服务器路径
+      pathRewrite: {
+        "^/mockxx/": "/"
+      } // 重写路径
+      //   changeOrigin: true
+    }
+  },
+  proxyTableLocal8888: {
+    "/api/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      // target: "http://172.16.4.146", // 真正测试服务器路径
+      target: "http://127.0.0.1:8888", // 本地服务器路径，已取消，使用mock代替
+      // target: 'http://172.16.4.194:8080/app/mock/21', //远端rap2模拟服务器路径
+      pathRewrite: {
+        "^/api/": "/"
+      }, // 重写路径
+
+      // changeOrigin: true,
+      bypass(req, res, proxyOptions) {
+        // 该函数必须返回false或返回被部署的文件路径，返回false继续。
+        const key = req.method.toUpperCase() + " " + req.url;
+        const urlPath = req.url.split("?")[0];
+        const mockPath = path.resolve(__dirname, "../mock/");
+        const fileName = path.resolve(mockPath, "./" + urlPath + ".json");
+        console.log("----------------request----------------", key);
+        // 是否需要本地模拟登陆
+        if (loginMock(req, res)) {
+          return true;
+        }
+        if (fs.existsSync(fileName)) {
+          var data = fs.readFileSync(fileName, "utf8"),
+            json = JSON.parse(data),
+            mockData = Mock.mock(json);
+          mockData.reqId = "mock-" + new Date().getTime();
+          console.log(
+            "--------------------mock----------------------\n",
+            fileName,
+            "mockData",
+            mockData
+          );
+          res.json(mockData);
+          return true;
+        }
+        return false;
+      }
+    },
+    "/mockxx/": {
+      // api表示当前项目请求使用该项可进入远程端访问
+      // target: "http://172.16.4.194:8080/app/mock/21", // 本地理服务器路径
+      target: "http://localhost:8080", // 本地理服务器路径
+      // target: 'http://140.143.26.135:3000', //远端 代理服务器路径
+      pathRewrite: {
+        "^/mockxx/": "/"
+      } // 重写路径
+      //   changeOrigin: true
+    }
+  }
+};
--- vue.config.js
+++ vue.config.js
@@ -1,37 +1,46 @@
+const devConfig = require("./config/dev.env");
+
+const proxyTableName = process.env.proxyTableName || "proxyTable";
+const proxyTable = devConfig[proxyTableName];
 // 基础路径 注意发布之前要先修改这里
-let baseUrl = './'
+let publicPath = "./";
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
-    baseUrl: baseUrl, // 根据你的实际情况更改这里
-    lintOnSave: true,
-    productionSourceMap: false,
-    // configureWebpack: config => {
-    //     if (process.env.NODE_ENV === 'production') {
-    //         return {
-    //             plugins: [
-    //                 new BundleAnalyzerPlugin()
-    //             ]
-    //         }
-    //     }
-    // },
-    chainWebpack: (config) => {
-        //忽略的打包文件
-        config.externals({
-            'vue': 'Vue',
-            'vue-router': 'VueRouter',
-            'vuex': 'Vuex',
-            'axios': 'axios',
-            'element-ui': 'ELEMENT',
-        })
-        const entry = config.entry('app')
-        entry
-            .add('babel-polyfill')
-            .end()
-        entry
-            .add('classlist-polyfill')
-            .end()
-        entry
-            .add('@/mock')
-            .end()
-    }
-}
+  publicPath: publicPath, // 根据你的实际情况更改这里
+  lintOnSave: true,
+  productionSourceMap: false,
+  devServer: {
+    // open: process.platform === 'darwin',
+    host: "0.0.0.0",
+    port: 8877,
+    https: false,
+    hotOnly: false,
+    disableHostCheck: true,
+    // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+    proxy: proxyTable, // string | Object
+    before: app => {}
+  },
+  // configureWebpack: config => {
+  //     if (process.env.NODE_ENV === 'production') {
+  //         return {
+  //             plugins: [
+  //                 new BundleAnalyzerPlugin()
+  //             ]
+  //         }
+  //     }
+  // },
+  chainWebpack: config => {
+    //忽略的打包文件
+    config.externals({
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      axios: "axios",
+      "element-ui": "ELEMENT"
+    });
+    const entry = config.entry("app");
+    entry.add("babel-polyfill").end();
+    entry.add("classlist-polyfill").end();
+    entry.add("@/mock").end();
+  }
+};
