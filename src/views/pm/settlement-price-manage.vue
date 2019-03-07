9f670f2cbca4f89b7f8af45b2e11f9cc1322cd68
SJ22CAN\Wings <sj22can@aliyun.com>
2019-03-07 21:58:27 +0800

    市场价、结算价管理页面，对话框布局待优化

---
+++ src/views/pm/settlement-price-manage.vue
@@ -0,0 +1,153 @@
+<template>
+  <basic-container>
+    <avue-crud
+      :option="option"
+      :page="page"
+      :data="data"
+      ref="crud1"
+    >
+      <template
+        slot-scope="scope"
+        slot="menuLeft"
+      >
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          size="small"
+          plain
+          @click="handleAdd()"
+        >新增</el-button>
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          size="small"
+          plain
+          @click="handleUpdate()"
+        >编辑</el-button>
+      </template>
+    </avue-crud>
+
+
+    <el-dialog
+      title
+      :visible.sync="dialog.visible"
+      width="80%"
+      :fullscreen="true"
+      :before-close="handleClose"
+    >
+      <div>
+        <avue-form
+          :option="dialog.formOption"
+          v-model="dialog.form"
+        ></avue-form>
+        <iframe
+          :src="url"
+          frameborder="0"
+        ></iframe>
+      </div>
+    </el-dialog>
+  </basic-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: {
+        visible: false,
+        ifrUrl: "https://form.avue.top",
+        form: { url: "" },
+        formOption: {
+          emptyBtn: false,
+          submitBtn: false,
+          column: [
+            
+            {
+              label: "产品大类",
+              prop: "prodCategories",
+              type: "select",
+              props: {
+                label: "prodCategories",
+                value: "id"
+              },
+              dicUrl: '/bd/product-categories/query',
+              rules: [
+                {
+                  required: true,
+                  message: "请选择产品大类 ",
+                  trigger: "blur"
+                }
+              ]
+            }
+
+          ]
+        }
+      },
+
+      page: {
+        total: 122
+      },
+      data: [
+        
+      ],
+      option: {
+        addBtn: false,
+        editBtn: false,
+        column: [
+          {
+            label: "产品大类",
+            prop: "prodCategories",
+            span: 14,
+            row: true
+          },
+          {
+            label: "最新结算价日期",
+            prop: "latestSettlementDate",
+            span: 14,
+            row: true
+          }
+          
+        ]
+      }
+    };
+  },
+  created(){
+      this.query()
+  },
+  computed:{
+      url:function(){
+          return 'http://172.16.2.245:8080/WebReport/ReportServer?reportlet=YKCS_SETTLEMENT_PRICE.cpt&op=write&pcid=1&aa='+this.dialog.form.prodCategories
+      }
+  },
+  methods: {
+    handleAdd() {
+      this.dialog.visible = true;
+    },
+    handleUpdate() {
+      this.dialog.visible = true;
+    },
+    handleClose() {
+      this.dialog.visible = false;
+    },
+    query(){
+        const self=this;
+        const data=[];
+        this.$$get("/mockxx/pm/settlement-price/queryLatestDateByProdCate").then(d=>{
+            
+            d.forEach(v=>data.push(v.tails));
+            self.data=data;
+        })
+    }
+  }
+};
+</script>
+
+<style>
+iframe {
+  display: block; /* iframes are inline by default */
+  border: none; /* Reset default border */
+  height: 100%; /* Viewport-relative units */
+  width: 100%;
+  min-height: calc(85vh - 10rem);
+}
+</style>
