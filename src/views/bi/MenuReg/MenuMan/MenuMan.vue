57b539ce22066d0ba39e13f490859f61bc86f16d
zztdan <zztdandanwork@gmail.com>
2019-03-11 10:54:11 +0800

    tmp2

--- src/views/bi/MenuReg/MenuMan/MenuMan.vue
+++ src/views/bi/MenuReg/MenuMan/MenuMan.vue
@@ -5,12 +5,7 @@
     <div class="flex-column">
       <div class="flex-item-8 main-height">
         <!-- 加载菜单树 -->
-        <el-tree
-          ref="menu-tree"
-          :default-expand-all="true"
-          :data="menuTree"
-          @node-click="handleNodeClick"
-        ></el-tree>
+        <el-tree ref="menu-tree" :default-expand-all="true" :data="menuTree"></el-tree>
       </div>
       <div class="flex-item-16 main-height"></div>
     </div>
@@ -24,71 +19,23 @@
   export default {
     name: "menu-man",
     components: {
-      LgDashboard,
-      MenuDetail
+      LgDashboard
       //   BuildForm
     },
     data: function() {
       //  将actDef转化为数组
 
       return {
-        hasChangedFlag: false,
-        menuTree: [],
-        menuNodeData: {},
-        menuNodeArray: menuEntity,
-        // 设定此处的操作为全部操作
-        actArr: [actDef.Add, actDef.AddChild, actDef.Edit, actDef.Del],
-        menuDetailType: "",
-        menuDetailTitle: ""
+        menuTree: []
       };
     },
-    computed: {
-      calcShowBtnGroup() {
-        if (typeof this.menuNodeData.name != "undefined") {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    created: function() {
+
+    created: async function() {
       //  debugger;
       let that_vue = this;
-
-      MenuListTree(that_vue)
-        .then(result => {
-          that_vue.menuTree = result;
-          that_vue.backTree = JSON.parse(JSON.stringify(result));
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      let res = await CRUD.queryMenu({});
     },
-    methods: {
-      handleNodeClick(data) {
-        this.menuNodeData = data;
-      },
-      handleNodeAction(key) {
-        //  this.menuDetailType = key;
-        this.$refs["menu-detail"].handleOpenDialog(key, this.menuNodeData);
-      },
-      // 当对话框关闭时重新向后台请求一次menu列表
-      handleDialogClosed() {
-        let that_vue = this;
-        this.$log("成功回调修改，加载菜单列表");
-        this.hasChangedFlag = true;
-        // 重新加载菜单列表
-        MenuListTree(that_vue)
-          .then(result => {
-            // debugger;
-            that_vue.menuTree = result;
-            that_vue.backTree = JSON.parse(JSON.stringify(result));
-          })
-          .catch(err => {
-            console.log(err);
-          });
-      }
-    }
+    methods: {}
   };
 </script>
 <style scoped>
